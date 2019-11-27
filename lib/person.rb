b46bb6899fcd897d13e42975855b4ed069349d6a
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-27 14:18:06 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,13 @@
 class Dog
- def initialize(name)
-   @name = name
-   @breed = breed
- end
-
+  def initialize(breed)
+    @breed = breed
+  end
+ 
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
 end
-
-
-dog_1 = Dog.new("Pluto")
